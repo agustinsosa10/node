3a9180848fc3c7703250669cf008e360cd221b9b
agustinsosa10 <118317303+agustinsosa10@users.noreply.github.com>
2025-12-19 00:35:56 -0300

    ejercicio 3.20

--- mongo.js
+++ mongo.js
@@ -20,6 +20,13 @@ const personSchema = new mongoose.Schema({
   },
   number:{
     type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: props => `${props.value} no es un numero valido`
+    },
     required: true
   } 
 });
